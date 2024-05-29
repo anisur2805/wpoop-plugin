ba745a1f7206af6a28abec0440501d9e497ea6af
Anisur Rahman <anisur2805@users.noreply.github.com>
2024-05-30 00:51:58 +0600

    Add custom fields

--- inc/API/SettingsAPI.php
+++ inc/API/SettingsAPI.php
@@ -2,9 +2,12 @@
 namespace WPOOP\API;
 
 class SettingsAPI {
-	public $admin_pages = array();
-
+	public $admin_pages    = array();
 	public $admin_subpages = array();
+
+	public $settings = array();
+	public $sections = array();
+	public $fields   = array();
 
 	public function register() {
 		if ( ! empty( $this->admin_pages ) ) {
@@ -66,6 +69,35 @@ class SettingsAPI {
 				$page['menu_slug'],
 				$page['callback']
 			);
+		}
+	}
+
+	public function set_settings( array $settings ) {
+		$this->settings = $settings;
+		return $this;
+	}
+
+	public function set_sections( array $sections ) {
+		$this->sections = $sections;
+		return $this;
+	}
+
+	public function set_fields( array $fields ) {
+		$this->fields = $fields;
+		return $this;
+	}
+
+	public function register_custom_fields() {
+		foreach ( $this->settings as $setting ) {
+			register_setting( $setting['option_group'], $setting['option_name'], ( isset( $setting['callback'] ) ? $setting['callback'] : '' ) );
+		}
+
+		foreach ( $this->sections as $section ) {
+			add_settings_section( $section['id'], $section['title'], ( isset( $section['callback'] ) ? $section['callback'] : '' ), $section['page'] );
+		}
+
+		foreach ( $this->fields as $field ) {
+			add_settings_field( $field['id'], $field['title'], ( isset( $field['callback'] ) ? $field['callback'] : '' ), $field['page'], $field['section'], ( isset( $field['args'] ) ? $field['args'] : '' ) );
 		}
 	}
 }
