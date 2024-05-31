bbb2932ad92283e6df0fd949da05d2a5d8a6af7b
Anisur Rahman <anisur2805@users.noreply.github.com>
2024-06-01 02:28:29 +0600

    ui-toggle add for checkbox

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -5,14 +5,14 @@ namespace WPOOP\Base;
 use WPOOP\Base\BaseController;
 
 class Enqueue extends BaseController {
-  
-    public function register() {
-        add_action('admin_enqueue_scripts', array( $this, 'enqueue' ) );
-    }
 
-    public function enqueue() {
-        wp_enqueue_style( 'wpoop-plugin-style', $this->plugin_url . 'assets/css/wpoop-plugin.css' );
-        wp_enqueue_script( 'wpoop-plugin-script', $this->plugin_url . 'assets/js/wpoop-plugin.js' );
-    }
+	public function register() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+	}
 
+	public function enqueue() {
+		wp_enqueue_style( 'wpoop-plugin-style', $this->plugin_url . 'assets/css/wpoop-plugin.css' );
+		wp_enqueue_style( 'ui-toggle', $this->plugin_url . 'assets/css/ui-toggle.css' );
+		wp_enqueue_script( 'wpoop-plugin-script', $this->plugin_url . 'assets/js/wpoop-plugin.js' );
+	}
 }
